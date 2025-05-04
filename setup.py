e0c1e3c6d5dd9d8223703b1bb261106ecd6903e1
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-05-04 18:33:16 +0500

    Setup completed

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import getpass
 import os
+from dotenv import load_dotenv
 
+# Load variables from .env into environment
+load_dotenv()
 
 def _set_env(var: str):
     if not os.environ.get(var):
         os.environ[var] = getpass.getpass(f"{var}: ")
-
 
 _set_env("ANTHROPIC_API_KEY")
